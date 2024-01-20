b5501db7d7b426195184fa0fa24941d9999e3db4
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2024-01-20 16:45:12 -0600

    mix new

---
+++ lib/dbg_issue.ex
@@ -0,0 +1,18 @@
+defmodule DbgIssue do
+  @moduledoc """
+  Documentation for `DbgIssue`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DbgIssue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dbg_issue_test.exs
@@ -0,0 +1,8 @@
+defmodule DbgIssueTest do
+  use ExUnit.Case
+  doctest DbgIssue
+
+  test "greets the world" do
+    assert DbgIssue.hello() == :world
+  end
+end
